0d2ae18cc1a14a5c86a8af15f3a1220111e64611
Manuel Izurieta <manuel@aaa.aaa>
2017-12-18 22:57:47 -0300

    Initial working release

--- src/main/java/com/test/rest/App.java
+++ src/main/java/com/test/rest/App.java
@@ -1,13 +1,22 @@
 package com.test.rest;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
 /**
  * Hello world!
  *
  */
+@EnableEurekaServer
+@SpringBootApplication
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
