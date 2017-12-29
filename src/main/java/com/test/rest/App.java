9cbdcd8f201269a6d2cdd593809bf3ebdf453da6
Manuel Izurieta <manuel@aaa.aaa>
2017-12-29 00:09:39 -0300

    Eureka completion

--- src/main/java/com/test/rest/App.java
+++ src/main/java/com/test/rest/App.java
@@ -4,14 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
-/**
- * Hello world!
- *
- */
 @EnableEurekaServer
 @SpringBootApplication
-
 public class App 
 {
     public static void main( String[] args )
